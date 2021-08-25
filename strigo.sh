c2e7cafa571c7b50e1d862359fc9269cef4092ac
Arul Vannala <arulvannala@avannala-a01.vmware.com>
2021-08-25 14:59:32 -0600

    updated

--- strigo.sh
+++ strigo.sh
@@ -14,7 +14,18 @@ chmod 700 get_helm.sh
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv ./kubectl /usr/local/bin/kubectl
-sudo -u ubuntu kind create cluster --name=springone2021
+sudo chown -R ubuntu:ubuntu /var/run/docker.sock
+cat << EOF > kind.yml
+# three node (workers) cluster config
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+- role: worker
+EOF
+sudo -u ubuntu kind create cluster --name=springone2021 --config=kind.yml
 sudo cp -r /root/.kube /$HOME/.kube
 sudo chown -R $USER $HOME/.kube
 while [[ $(kubectl get node -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for Nodes to get Ready" && sleep 1; done
@@ -27,26 +38,26 @@ kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2
 while [[ $(kubectl get pods -n cert-manager -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True" ]]; do echo "Checking for cert-manager pods" && sleep 1; done
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install my-release bitnami/spring-cloud-dataflow
-kubectl create secret docker-registry scdf-image-regcred --docker-server=registry.pivotal.io --docker-username=avannala@pivotal.io --docker-password=$dockerpasss
-kubectl create ns gemfire-system
-kubectl create secret docker-registry image-pull-secret --namespace=gemfire-system --docker-server=registry.pivotal.io --docker-username=avannala@pivotal.io --docker-password=$dockerpasss
-kubectl create secret docker-registry image-pull-secret --docker-server=registry.pivotal.io --docker-username=avannala@pivotal.io --docker-password=$dockerpasss
-export HELM_EXPERIMENTAL_OCI=1
-helm registry login -u myuser registry.pivotal.io -u avannala@pivotal.io -p $dockerpasss
-helm repo update
-helm chart pull registry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-operator:1.0.1
-helm chart export registry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-operator:1.0.1
-helm install gemfire-operator gemfire-operator --namespace gemfire-system
-while [[ $(kubectl get po -n gemfire-system -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for podes to get Ready" && sleep 1; done
-helm ls --namespace gemfire-system
-cat << EOF > gemfire.yml
-apiVersion: gemfire.tanzu.vmware.com/v1
-kind: GemFireCluster
-metadata:
-  name: gemfire1
-spec:
-  image: registry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-k8s:1.0.1
-EOF
-kubectl apply -f gemfire.yml
+kubectl create secret docker-registry scdf-image-regcred --docker-server=registry.pivotal.io --docker-username=avannala@pivotal.io --docker-password=1@Universe9
+#kubectl create ns gemfire-system
+#kubectl create secret docker-registry image-pull-secret --namespace=gemfire-system --docker-server=registry.pivotal.io --docker-username=avannala@pivotal.io --docker-password=$dockerpasss
+#kubectl create secret docker-registry image-pull-secret --docker-server=registry.pivotal.io --docker-username=avannala@pivotal.io --docker-password=$dockerpasss
+#export HELM_EXPERIMENTAL_OCI=1
+#helm registry login -u myuser registry.pivotal.io -u avannala@pivotal.io -p $dockerpasss
+#helm repo update
+#helm chart pull registry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-operator:1.0.1
+#helm chart export registry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-operator:1.0.1
+#helm install gemfire-operator gemfire-operator --namespace gemfire-system
+#while [[ $(kubectl get po -n gemfire-system -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for podes to get Ready" && sleep 1; done
+#helm ls --namespace gemfire-system
+#cat << EOF > gemfire.yml
+#apiVersion: gemfire.tanzu.vmware.com/v1
+#kind: GemFireCluster
+#metadata:
+#  name: gemfire1
+#spec:
+#  image: registry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-k8s:1.0.1
+#EOF
+#kubectl apply -f gemfire.yml
 kubectl get ns
 kubectl get pods -A
